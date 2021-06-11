50cd744c1e6211546d475f5de5263d0e5f4121fd
Kyle Misner <ismakefire@gmail.com>
2021-06-11 12:49:48 -0700

    Quick code quality update with some fundamental changes too. Vector: -New hash function, and string functions. -Component comments, fixes to deprecated uses of ‘<<’ and in comments. -White space update (Cut-all, paste-all). Dictionary: -Regions, comments -Fix load exception message -White space update (Cut-all, paste-all).

--- Scripts/Collections/SerializableDictionary.cs
+++ Scripts/Collections/SerializableDictionary.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 using System;
 
@@ -9,39 +8,52 @@ using System;
 * 
 * A serializable dictionary class.
 */
-namespace christophfranke123 {
+namespace christophfranke123
+{
+    [Serializable]
+    public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+    {
+        #region Variables
+        [SerializeField]
+        private List<TKey> keys = new List<TKey>();
+        
+        [SerializeField]
+        private List<TValue> values = new List<TValue>();
+        #endregion
 
-	[Serializable]
-	public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
-	{
-		[SerializeField]
-		private List<TKey> keys = new List<TKey>();
 
-		[SerializeField]
-		private List<TValue> values = new List<TValue>();
-
-		// save the dictionary to lists
-		public void OnBeforeSerialize()
-		{
-			keys.Clear();
-			values.Clear();
-			foreach(KeyValuePair<TKey, TValue> pair in this)
-			{
-				keys.Add(pair.Key);
-				values.Add(pair.Value);
-			}
-		}
-
-		// load dictionary from lists
-		public void OnAfterDeserialize()
-		{
-			this.Clear();
-
-			if(keys.Count != values.Count)
-				throw new System.Exception(string.Format("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable."));
-
-			for(int i = 0; i < keys.Count; i++)
-				this.Add(keys[i], values[i]);
-		}
-	}
+        #region ISerializationCallbackReceiver
+        /// <summary>
+        /// Save the dictionary to lists.
+        /// </summary>
+        public void OnBeforeSerialize()
+        {
+            keys.Clear();
+            values.Clear();
+            foreach (KeyValuePair<TKey, TValue> pair in this)
+            {
+                keys.Add(pair.Key);
+                values.Add(pair.Value);
+            }
+        }
+        
+        /// <summary>
+        /// Load dictionary from lists.
+        /// </summary>
+        public void OnAfterDeserialize()
+        {
+            this.Clear();
+            
+            if (keys.Count != values.Count)
+            {
+                throw new System.Exception(string.Format("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable.", keys.Count, values.Count));
+            }
+            
+            for (int i = 0; i < keys.Count; i++)
+            {
+                this.Add(keys[i], values[i]);
+            }
+        }
+        #endregion
+    }
 }
--- Scripts/Vector/IntVector2.cs
+++ Scripts/Vector/IntVector2.cs
@@ -6,133 +6,169 @@
  */
 
 [System.Serializable]
-public struct IntVector2 {
-	
-	public int x;
-	public int y;
-	
-	/// <summary>
-	/// Shorthand for writing new IntVector2(0, 0).
-	/// </summary>
-	public static readonly IntVector2 zero = new IntVector2(0, 0);
-	
-	/// <summary>
-	/// Shorthand for writing new IntVector2(1, 1).
-	/// </summary>
-	public static readonly IntVector2 one = new IntVector2(1, 1);
-	
-	/// <summary>
-	/// Constructs a new vector with given x, y components.
-	/// </summary>
-	public IntVector2 (int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	public static IntVector2 operator +(IntVector2 left, IntVector2 right) {
-		left.x += right.x;
-		left.y += right.y;
-		return left;
-	}
-	
-	public static IntVector2 operator -(IntVector2 left, IntVector2 right) {
-		left.x -= right.x;
-		left.y -= right.y;
-		return left;
-	}
-	
-	public static IntVector2 operator -(IntVector2 value) {
-		value.x = -value.x;
-		value.y = -value.y;
-		return value;
-	}
-	
-	public static IntVector2 operator *(IntVector2 left, int rightScale) {
-		left.x *= rightScale;
-		left.y *= rightScale;
-		return left;
-	}
-	
-	public static IntVector2 operator *(int leftScale, IntVector2 right) {
-		
-		// This assumes the communitative property, a * b = b * a.
-		right.x *= leftScale;
-		right.y *= leftScale;
-		return right;
-	}
-	
-	public static IntVector2 operator /(IntVector2 left, int rightScale) {
-		left.x /= rightScale;
-		left.y /= rightScale;
-		return left;
-	}
-	
-	public static IntVector2 operator %(IntVector2 left, int rightScale) {
-		left.x %= rightScale;
-		left.y %= rightScale;
-		return left;
-	}
-	
-	public static bool operator ==(IntVector2 left, IntVector2 right) {
-		return EqualsInternal(left, right);
-	}
-	
-	public static bool operator !=(IntVector2 left, IntVector2 right) {
-		return (EqualsInternal(left, right) == false);
-	}
+public struct IntVector2
+{
+    /// <summary>
+    ///   <para>X component of the vector.</para>
+    /// </summary>
+    public int x;
 
-	public override bool Equals(object o) {
+    /// <summary>
+    ///   <para>Y component of the vector.</para>
+    /// </summary>
+    public int y;
 
-		if ( !(o is IntVector2) ) {
-			return false;
-		}
+    /// <summary>
+    /// Shorthand for writing new IntVector2(0, 0).
+    /// </summary>
+    public static readonly IntVector2 zero = new IntVector2(0, 0);
 
-		IntVector2 left = this;
-		IntVector2 right = (IntVector2)o;
+    /// <summary>
+    /// Shorthand for writing new IntVector2(1, 1).
+    /// </summary>
+    public static readonly IntVector2 one = new IntVector2(1, 1);
 
-		return EqualsInternal(left, right);
-	}
+    /// <summary>
+    /// Constructs a new vector with given x, y components.
+    /// </summary>
+    public IntVector2(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
 
-	/// <summary>
-	/// Serves as a hash function for a IntVector2.
-	/// 
-	/// Why this implementaiton over others?
-	/// 
-	/// So the other implementations I've seen and considered are either as fast as possilbe or an even distribution.
-	/// 
-	/// First let's consider our fast implementations. Modern CPUs care more about operation alignment then the
-	/// performance of a single operation, so it's likely that XOR and Multiplication are each faster then a pair of
-	/// operations. Assuming this, I ran some some tests and found Subtraction and Multiplication beats out XOR and
-	/// Addition for small values and positive values. Multiplication has a pretty big flaw with data sets with a
-	/// large number of zeros. So ultimatedly, Subtraction ended up being my favorite choice.
-	/// 
-	/// Next, when looking at even distributions we're clearly gonna do great for chess boards, but what about
-	/// completely noisey values or byte aligned values? For implementations of the form x ^ (y << N), N = 16 feels
-	/// like an easy choise, but you're assuming the complexity of y comes from it's bottom half, which might not be
-	/// the case if we're converting from floats (a common case) or byte packed values (32 bit colors).
-	/// 
-	/// So for me, N = 13 strikes a fair balance between (x ^ (y << 16)) low order perfection and a rotating shift
-	/// implementation of the form (x ^ (y << N) ^ (y >> N)), while also avoiding common byte alignments of 8 bits.
-	/// 
-	/// Finally, combining the solutions into a form such as the following: x - (y << 13), sounds reasonable but
-	/// would be missleading without data supporting it. So I'm sticking with what's a more readable choice unless
-	/// I find further data to do otherwise.
-	/// 
-	/// </summary>
-	/// <returns>A hash code for this instance that is suitable for use in hashing algorithms and data structures such as a hash table.</returns>
-	public override int GetHashCode ()
-	{
-		return x ^ (y << 13);
-	}
+    public static IntVector2 operator +(IntVector2 left, IntVector2 right)
+    {
+        left.x += right.x;
+        left.y += right.y;
+        return left;
+    }
 
-	private static bool EqualsInternal(IntVector2 left, IntVector2 right) {
-		return (left.x == right.x && left.y == right.y);
-	}
-	
-	/// <summary>
-	/// Returns a nicely formatted string for this vector.
-	/// </summary>
-	public override string ToString() {
-		return string.Format("({0}, {1})", x, y);
-	}
+    public static IntVector2 operator -(IntVector2 left, IntVector2 right)
+    {
+        left.x -= right.x;
+        left.y -= right.y;
+        return left;
+    }
+
+    public static IntVector2 operator -(IntVector2 value)
+    {
+        value.x = -value.x;
+        value.y = -value.y;
+        return value;
+    }
+
+    public static IntVector2 operator *(IntVector2 left, int rightScale)
+    {
+        left.x *= rightScale;
+        left.y *= rightScale;
+        return left;
+    }
+
+    public static IntVector2 operator *(int leftScale, IntVector2 right)
+    {
+
+        // This assumes the communitative property, a * b = b * a.
+        right.x *= leftScale;
+        right.y *= leftScale;
+        return right;
+    }
+
+    public static IntVector2 operator /(IntVector2 left, int rightScale)
+    {
+        left.x /= rightScale;
+        left.y /= rightScale;
+        return left;
+    }
+
+    public static IntVector2 operator %(IntVector2 left, int rightScale)
+    {
+        left.x %= rightScale;
+        left.y %= rightScale;
+        return left;
+    }
+
+    public static bool operator ==(IntVector2 left, IntVector2 right)
+    {
+        return EqualsInternal(left, right);
+    }
+
+    public static bool operator !=(IntVector2 left, IntVector2 right)
+    {
+        return (EqualsInternal(left, right) == false);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (!(obj is IntVector2))
+        {
+            return false;
+        }
+
+        IntVector2 left = this;
+        IntVector2 right = (IntVector2)obj;
+
+        return EqualsInternal(left, right);
+    }
+
+    /// <summary>
+    /// Serves as a hash function for a IntVector2.
+    /// 
+    /// Switching to an approch used by unity, which beats out our old
+    /// implementation across the board. Leaving in our old explaination.
+    /// 
+    /// Old explaination:
+    /// Why this implementaiton over others?
+    /// 
+    /// So the other implementations I've seen and considered are either as fast as possilbe or an even distribution.
+    /// 
+    /// First let's consider our fast implementations. Modern CPUs care more about operation alignment then the
+    /// performance of a single operation, so it's likely that XOR and Multiplication are each faster then a pair of
+    /// operations. Assuming this, I ran some some tests and found Subtraction and Multiplication beats out XOR and
+    /// Addition for small values and positive values. Multiplication has a pretty big flaw with data sets with a
+    /// large number of zeros. So ultimatedly, Subtraction ended up being my favorite choice.
+    /// 
+    /// Next, when looking at even distributions we're clearly gonna do great for chess boards, but what about
+    /// completely noisey values or byte aligned values? For implementations of the form x ^ (y [left] N), N = 16 feels
+    /// like an easy choise, but you're assuming the complexity of y comes from it's bottom half, which might not be
+    /// the case if we're converting from floats (a common case) or byte packed values (32 bit colors).
+    /// 
+    /// So for me, N = 13 strikes a fair balance between (x ^ (y [left] 16)) low order perfection and a rotating shift
+    /// implementation of the form (x ^ (y [left] N) ^ (y [right] N)), while also avoiding common byte alignments of 8 bits.
+    /// 
+    /// Finally, combining the solutions into a form such as the following: x - (y [left] 13), sounds reasonable but
+    /// would be missleading without data supporting it. So I'm sticking with what's a more readable choice unless
+    /// I find further data to do otherwise.
+    /// 
+    /// Note: I have replaced all uses of the "left bit-shift operator" and the "right bit-shift operator" have been replaces
+    /// with [left] and [right] respectively.
+    /// 
+    /// </summary>
+    /// <returns>A hash code for this instance that is suitable for use in hashing algorithms and data structures such as a hash table.</returns>
+    public override int GetHashCode()
+    {
+        // Old: return x ^ (y << 13);
+        return x.GetHashCode() ^ (y.GetHashCode() << 2);
+    }
+
+    private static bool EqualsInternal(IntVector2 left, IntVector2 right)
+    {
+        return (left.x == right.x && left.y == right.y);
+    }
+
+    /// <summary>
+    /// Returns a nicely formatted string for this vector.
+    /// </summary>
+    public override string ToString()
+    {
+        return string.Format("({0:F1}, {1:F1})", x, y);
+    }
+
+    /// <summary>
+    /// Returns a nicely formatted string for this vector.
+    /// </summary>
+    public string ToString(string format)
+    {
+        return string.Format("({0}, {1})", x.ToString(format), y.ToString(format));
+    }
 }
